e49998ea8d106de8a1cc08cedfbd77f2bb8b6f60
DaveAlsina <davealsina@gmail.com>
2023-01-07 22:47:59 -0500

    update, the default user needed create privileges on the schema

--- create_db_usr.sh
+++ create_db_usr.sh
@@ -5,11 +5,12 @@ sudo systemctl enable postgresql
 #sudo -i -u postgres
 sudo -u postgres psql
 create user dave with encrypted password '0000';
-grant all privileges on database maticas to dave;
+grant create on schema public to dave;
 
 ALTER USER dave CREATEDB;
 
 create database maticas;
+grant all privileges on database maticas to dave;
 create database maticas_users;
 
 
